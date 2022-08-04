ed356c372f58d066825379eacb2ca9b08cd1654a
Arian94 <arianpourarian@gmail.com>
2022-08-04 20:28:45 +0430

    add keyboard listener for translations - improve ui/ux

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { readText } from '@tauri-apps/api/clipboard';
 import { listen } from '@tauri-apps/api/event';
 import { invoke } from '@tauri-apps/api/tauri';
-import { createRef, useEffect, useRef, useState } from 'react';
+import { createRef, MutableRefObject, useEffect, useRef, useState } from 'react';
 import './App.scss';
 import './assets/fonts/persian/NotoSansArabic.ttf';
 import { google_translate_icon } from './assets/images';
@@ -12,26 +12,46 @@ type CountriesValues = typeof countries[CountriesKeys];
 
 // todo save configs and settings
 function App() {
-  const [translation, setTranslation] = useState("");
-  const [value, setValue] = useState("");
+  const translationSpeakHandler = function (e: KeyboardEvent) {
+    if (!translationRef.current) return;
+    if (toRef.current === 'fa') return;
+    if (e.key !== 'Enter' || !e.ctrlKey) return;
+    speak(translationRef.current, toRef.current);
+  }
+
+  const inputSpeakHandler = function (this: HTMLInputElement, e: KeyboardEvent) {
+    if (e.key !== 'Enter') return;
+    if (e.ctrlKey) return;
+    const { value } = this;
+    speak(value, fromRef.current);
+  }
+
+  const [inputVal, setInputVal] = useState("");
   const [activeTab, setActiveTab] = useState<'online' | 'offline'>('online');
+  const fromRef = useRef<CountriesValues | 'auto'>('auto');
   const [from, setFrom] = useState<CountriesValues | 'auto'>('auto');
+  const toRef = useRef<CountriesValues>('fa');
   const [to, setTo] = useState<CountriesValues>('fa');
+  const translationRef = useRef('');
   const [loading, setLoading] = useState<boolean>(false);
-  const enterHandler = useRef<(this: HTMLInputElement, ev: KeyboardEvent) => any>();
-  const inputElm = createRef<HTMLInputElement>();
+  const inputRef = createRef<HTMLInputElement>();
   let clipboardBuffer: string | null;
 
+  const setRefCurrent = (ref: MutableRefObject<string>, value: string) => {
+    ref.current = value
+  }
+
   useEffect(() => {
+    inputRef.current?.addEventListener('keypress', inputSpeakHandler);
+    window.addEventListener('keypress', translationSpeakHandler);
+
     const readClipboard = (clip: string | null) => {
       if (!clip?.trim()) return;
       if (clip === clipboardBuffer) return;
       if (clip.search(/[;{}\[\]<>]/) >= 0) return;
 
       clipboardBuffer = clip;
-      if (!inputElm.current) return;
-      inputElm.current.value = clip;
-      setValue(clip);
+      setInputVal(clip);
     }
 
     // run readText once to store/read clipboard content which may exist before opening the app. 
@@ -43,22 +63,19 @@ function App() {
   }, []);
 
   useEffect(() => {
-    if (enterHandler.current)
-      inputElm.current?.removeEventListener('keypress', enterHandler.current);
-    if (from === 'fa') return;
+    const timeOutId = setTimeout(() => handler(), 700);
+    return () => clearTimeout(timeOutId);
+  }, [inputVal]);
 
-    enterHandler.current = function (e: KeyboardEvent) {
-      if (e.key !== 'Enter') return;
-      const { value } = this;
-      speak(value, from);
-    }
-    inputElm.current?.addEventListener('keypress', enterHandler.current);
+  useEffect(() => {
+    fromRef.current = from;
+    setTimeout(() => handler(), 0);
   }, [from])
 
   useEffect(() => {
-    const timeOutId = setTimeout(() => handler(), 500);
-    return () => clearTimeout(timeOutId);
-  }, [value, from, to]);
+    toRef.current = to;
+    setTimeout(() => handler(), 0);
+  }, [to])
 
   const langOptions = (option: 'from' | 'to') => {
     const ops: JSX.IntrinsicElements['option'][] = [];
@@ -71,8 +88,9 @@ function App() {
   }
 
   const swapLang = () => {
-    setFrom(to);
-    from === 'auto' ? setTo('en') : setTo(from);
+    setFrom(to)
+    setTo(from === 'auto' ? to === 'en' ? 'fr' : 'en' : from)
+    setTimeout(() => handler(), 0);
   }
 
   const speak = (word: string, lang: CountriesValues | 'auto') => {
@@ -80,25 +98,23 @@ function App() {
   }
 
   const invokeBackend = async () => {
-    let translation = '';
+    let translationVal = '';
     if (activeTab === 'online')
-      translation = await invoke<string>('google_translate', { from, to, word: value });
+      translationVal = await invoke<string>('google_translate', { from: fromRef.current, to: toRef.current, word: inputVal });
     else {
       try {
-        const dt = await invoke<string>('find', { word: value });
-        translation = dt;
+        const dt = await invoke<string>('find', { word: inputVal });
+        translationVal = dt;
       } catch (er: any) {
-        translation = er;
+        translationVal = er;
       }
     }
-
-    setTranslation(translation)
+    setRefCurrent(translationRef, translationVal)
     setLoading(false)
   }
 
   const handler = () => {
-    if (!value.trim()) return;
-    if (!isNaN(+value)) return;
+    if (!inputVal.trim() || !isNaN(+inputVal)) return;
     setLoading(true);
     invokeBackend();
   };
@@ -106,11 +122,11 @@ function App() {
   return (
     <div className="App">
       <ul className="nav">
-        <li className={activeTab === "online" ? "active" : ""} onClick={() => { setActiveTab('online'); setTranslation('') }}>
+        <li className={activeTab === "online" ? "active" : ""} onClick={() => { setActiveTab('online'); setRefCurrent(translationRef, '') }}>
           Online
           <img src={google_translate_icon} />
         </li>
-        <li className={activeTab === "offline" ? "active" : ""} onClick={() => { setActiveTab('offline'); setTranslation('') }}>
+        <li className={activeTab === "offline" ? "active" : ""} onClick={() => { setActiveTab('offline'); setRefCurrent(translationRef, '') }}>
           En-Fa <sup>(Offline)</sup>
         </li>
       </ul>
@@ -119,7 +135,7 @@ function App() {
         <div className="language-options">
           <div className="from">
             <span>from</span>
-            <select key="from" onChange={event => setFrom(event.target.value as CountriesValues)} value={from}>
+            <select key="from" value={from} onChange={event => setFrom(event.target.value as CountriesValues)}>
               <option value="auto">Detect</option>
               <>
                 {langOptions('from')}
@@ -131,8 +147,7 @@ function App() {
 
           <div className="to">
             <span>to</span>
-            <select key="to"
-              onChange={event => setTo(event.target.value as CountriesValues)} value={to}>
+            <select key="to" value={to} onChange={event => setTo(event.target.value as CountriesValues)}>
               <>
                 {langOptions('to')}
               </>
@@ -142,16 +157,29 @@ function App() {
       }
 
       <div className="input">
-        <input ref={inputElm} autoFocus placeholder='search...' onChange={event => setValue(event.target.value)} />
-        <button onClick={() => speak(value, from)} style={{ opacity: !value || from === 'fa' ? .5 : 1 }} disabled={!value || from === 'fa'}></button>
+        <input ref={inputRef} autoFocus placeholder={from === 'fa' ? 'جستجو...' : 'search...'} value={inputVal} onInput={event => setInputVal(event.currentTarget.value)}
+          style={{
+            direction: from === 'fa' ? 'rtl' : 'ltr',
+            fontFamily: from === 'fa' ? 'PersianFont' : 'inherit',
+            fontSize: from === 'fa' ? '10px' : 'inherit'
+          }} />
+        <button onClick={() => speak(inputVal, from)}
+          style={{
+            opacity: !inputVal || from === 'fa' ? .5 : 1, left: from !== 'fa' ? '2px' : 'unset', right: from !== 'fa' ? 'unset' : '2px',
+            transform: from === 'fa' ? 'scaleX(-1)' : 'unset'
+          }}
+          disabled={!inputVal || from === 'fa'}>
+        </button>
       </div>
 
       <fieldset className='translation' dir={to === 'fa' ? 'rtl' : 'ltr'} style={{ opacity: loading ? .5 : 1 }}>
         <legend>
           Translation
-          <button onClick={() => speak(translation, to)} style={{ display: !translation || to === 'fa' ? 'none' : 'block' }} disabled={!translation || to === 'fa'}></button>
+          <button onClick={() => speak(translationRef.current, to)} style={{ display: !translationRef.current || to === 'fa' ? 'none' : 'block' }}
+            disabled={!translationRef || to === 'fa'}>
+          </button>
         </legend>
-        {translation}
+        {translationRef.current}
       </fieldset>
     </div>
   )
